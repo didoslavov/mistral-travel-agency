3cd7f83c142cd8200368bda24d6603eca77b9fd5
didoslavov <didkoslawow@gmail.com>
2024-04-20 14:39:04 +0300

    add content

---
+++ src/components/Content.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import About from "./About/About";
+
+function Content() {
+  return (
+    <section className="col-span-6 col-start-3 row-start-3 w-[78%] -translate-y-24 ps-14 pt-40">
+      <About />
+    </section>
+  );
+}
+
+export default Content;
