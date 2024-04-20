7b645bfdea746285cffe41dc0a59eecebbaa6b94
didoslavov <didkoslawow@gmail.com>
2024-04-20 14:38:37 +0300

    add program

---
+++ src/components/Program/Program.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Program() {
+  return (
+    <section className="bg-main-grey-100 col-span-full col-start-7 row-start-3 ml-24 -translate-y-24 ps-20 pt-40">
+      Program
+    </section>
+  );
+}
+
+export default Program;
