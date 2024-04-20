21909eb23738a5c77c4c34cfd4b995dcf793e4dc
didoslavov <didkoslawow@gmail.com>
2024-04-20 14:39:32 +0300

    add main sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+import Content from "@/components/Content";
 import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
+import Program from "@/components/Program/Program";
 
 export default function Home() {
   return (
     <main className="grid grid-cols-12">
       <Header />
       <Hero />
+      <Content />
+      <Program />
     </main>
   );
 }
