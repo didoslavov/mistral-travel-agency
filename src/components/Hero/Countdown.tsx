169a26de00ba389d21bc29d55c7a255bc9cd2fba
didoslavov <didkoslawow@gmail.com>
2024-04-20 04:26:53 +0300

    fix styles

--- src/components/Hero/Countdown.tsx
+++ src/components/Hero/Countdown.tsx
@@ -3,21 +3,21 @@ import React from "react";
 
 function Countdown() {
   return (
-    <div className="mb-12 flex items-center gap-20 text-[#FEFEFE]">
+    <div className="gap mb-10 flex items-center gap-x-[82px] text-[#FEFEFE]">
       <div className="flex flex-col items-center">
         <time className="text-[58px]" dateTime={`P${11}D`}>
           {11}
         </time>
         <span className="text-[14px]">дни</span>
       </div>
-      <span className="h-[50%] w-[3px] -translate-y-1 bg-[#FEFEFE]"></span>
+      <span className="h-[50%] w-[4px] -translate-y-1 bg-[#FEFEFE]"></span>
       <div className="flex flex-col items-center">
         <time className="text-[58px]" dateTime={`PT${14}H`}>
           {14}
         </time>
         <span className="text-[14px]">часа</span>
       </div>
-      <span className="h-[50%] w-[3px] -translate-y-1 bg-[#FEFEFE]"></span>
+      <span className="h-[50%] w-[4px] -translate-y-1 bg-[#FEFEFE]"></span>
       <div className="flex flex-col items-center">
         <time className="text-[58px]" dateTime={`PT${55}M`}>
           {55}
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -3,18 +3,20 @@ import InfoCards from "./InfoCards";
 
 function Hero() {
   return (
-    <section className="row-start-2 flex w-screen  flex-col items-center bg-[url(/hero-bg.png)] bg-cover bg-bottom text-[#FFFFFE]">
-      <h1 className="mb-[16px] mt-[90px] text-[47px] font-extrabold">
+    <section className="row-start-2  flex w-screen flex-col items-center bg-[url(/hero-bg.png)] bg-cover bg-bottom text-[#FFFFFE]">
+      <h1 className="mb-[10px] mt-[90px] text-[47px] font-extrabold">
         Арабска приказка в Йордания
       </h1>
-      <p className="mb-[74px] border-2 border-white px-[30px] py-[6px] text-xl">
+      <p className="mb-[70px] border-2 border-white px-[32px] py-[7px] text-xl">
         11-15 Март
       </p>
       <Countdown />
       <button className="bg-main-red mb-3 rounded-[27.5px] px-7 py-[14px] font-bold leading-[25px]">
         Резервирайте сега
       </button>
-      <p className="mb-16 text-sm text-[#FEFEFE]">от 1149,00лв.</p>
+      <p className="mb-16 text-sm text-[#FEFEFE] opacity-80">
+        от <span className="mr-1">1</span>149,00лв.
+      </p>
       <InfoCards />
     </section>
   );
