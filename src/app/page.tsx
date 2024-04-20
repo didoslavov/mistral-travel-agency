93ffb8247e6e3f40a78ee388bbabcf85ff82c90d
didoslavov <didkoslawow@gmail.com>
2024-04-20 03:26:42 +0300

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
+
 export default function Home() {
-    return <main className=""></main>;
+  return (
+    <main className="grid grid-cols-12">
+      <Header />
+      <Hero />
+    </main>
+  );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,23 @@
+import Countdown from "./Countdown";
+import InfoCards from "./InfoCards";
+
+function Hero() {
+  return (
+    <section className="row-start-2 flex w-screen  flex-col items-center bg-[url(/hero-bg.png)] bg-cover bg-bottom text-[#FFFFFE]">
+      <h1 className="mb-[16px] mt-[90px] text-[47px] font-extrabold">
+        Арабска приказка в Йордания
+      </h1>
+      <p className="mb-[74px] border-2 border-white px-[30px] py-[6px] text-xl">
+        11-15 Март
+      </p>
+      <Countdown />
+      <button className="bg-main-red mb-3 rounded-[27.5px] px-7 py-[14px] font-bold leading-[25px]">
+        Резервирайте сега
+      </button>
+      <p className="mb-16 text-sm text-[#FEFEFE]">от 1149,00лв.</p>
+      <InfoCards />
+    </section>
+  );
+}
+
+export default Hero;
