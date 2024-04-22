813aa5ae3ea2f7d2bec3ec5285d2e13f92da74c2
didoslavov <didkoslawow@gmail.com>
2024-04-22 23:43:54 +0300

    fix styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,14 +8,14 @@ function Header() {
   return (
     <header className="grid grid-cols-12">
       <div className="col-span-8 col-start-3 flex w-full items-center justify-between pb-3 pt-3 text-main-grey">
-        <div className="flex items-center gap-6">
-          <FaBars
-            size={30}
-            className="h-[30px] w-[27px] font-thin leading-loose hover:cursor-pointer"
-          />
+        <div className="ps-12">
           <Image src={logo} alt="Лого" className="-translate-y-3" />
         </div>
         <Nav />
+        <FaBars
+          size={30}
+          className="h-[30px] w-[27px] hover:cursor-pointer md:hidden"
+        />
       </div>
     </header>
   );
