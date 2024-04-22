d4abfac0e5c9c08141d6cf40e05c4419e95eb63c
didoslavov <didkoslawow@gmail.com>
2024-04-23 00:46:32 +0300

    add responsive styles header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,35 @@
+"use client";
+
 import { FaBars } from "react-icons/fa";
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Nav";
 import Image from "next/image";
 import logo from "/public/logo.png";
+import { IoCloseSharp } from "react-icons/io5";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <header className="grid grid-cols-12">
-      <div className="col-span-8 col-start-3 flex w-full items-center justify-between pb-3 pt-3 text-main-grey">
-        <div className="ps-12">
+    <header className="grid grid-cols-12 px-4">
+      <div className="col-span-full col-start-1 flex w-full items-center justify-between pb-3 pt-3 text-main-grey xl:col-span-8 xl:col-start-3">
+        <div className="relative lg:ps-12">
           <Image src={logo} alt="Лого" className="-translate-y-3" />
         </div>
-        <Nav />
-        <FaBars
-          size={30}
-          className="h-[30px] w-[27px] hover:cursor-pointer md:hidden"
-        />
+        <Nav isOpen={isOpen} />
+        {isOpen ? (
+          <IoCloseSharp
+            size={30}
+            className="z-50 block stroke-main-grey hover:cursor-pointer md:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        ) : (
+          <FaBars
+            size={30}
+            className="block stroke-main-grey hover:cursor-pointer md:hidden"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        )}
       </div>
     </header>
   );
--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import NavLink from "../NavLink/NavLink";
 import { headerLinks } from "@/data/headerData";
 
-function Nav() {
+function Nav({ isOpen }: { isOpen: boolean }) {
   return (
-    <div>
-      <ul className="flex gap-10 text-[15px] font-bold leading-[25px]">
+    <div
+      className={`items-center md:flex ${isOpen ? "fixed right-0 top-0 z-50 flex h-full w-full justify-center bg-main-grey-200 text-main-grey" : "hidden"}`}
+    >
+      <ul
+        className={`${isOpen ? "text-sm md:text-xl" : "text-xs"} flex flex-col justify-center gap-6 font-bold leading-[25px] md:flex-row lg:text-[15px] xl:gap-10`}
+      >
         {headerLinks.map((l, i) => (
           <NavLink key={i}>{l}</NavLink>
         ))}
