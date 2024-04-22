3ed571c1a7ecc86a0f48d8fc7561f6a6fed79ce7
didoslavov <didkoslawow@gmail.com>
2024-04-22 23:44:50 +0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header/Header";
 import Hero from "@/components/Hero/Hero";
 import Offers from "@/components/Offers/Offers";
 import Partners from "@/components/Partners/Partners";
-import Program from "@/components/Program/Program";
+import Program from "@/components/Content/Program/Program";
 
 export default function Home() {
   return (
@@ -14,7 +14,6 @@ export default function Home() {
       <main className="grid grid-cols-12">
         <Hero />
         <Content />
-        <Program />
         <Offers />
         <Partners />
         <Cta />
