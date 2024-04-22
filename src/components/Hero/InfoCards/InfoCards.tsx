d2dc1c17814fa2550b8226b5a987ee85170c350c
didoslavov <didkoslawow@gmail.com>
2024-04-22 23:44:34 +0300

    fix styles

--- src/components/Hero/InfoCards/InfoCards.tsx
+++ src/components/Hero/InfoCards/InfoCards.tsx
@@ -3,7 +3,7 @@ import { cards } from "@/data/heroData";
 
 function InfoCards() {
   return (
-    <div className="flex -translate-y-9 gap-[10px]">
+    <div className="absolute bottom-0 z-50 col-start-4 row-start-3 mt-12 flex gap-[10px] pt-28">
       {cards.map((c, i) => (
         <InfoCard text={c.text} Icon={c.icon} key={i} />
       ))}
