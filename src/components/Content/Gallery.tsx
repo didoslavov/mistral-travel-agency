1b56205565dc3a4155068e7b2a01105bc0196ed2
didoslavov <didkoslawow@gmail.com>
2024-04-22 23:43:21 +0300

    refactor

--- src/components/Content/Gallery.tsx
+++ src/components/Content/Gallery.tsx
@@ -5,58 +5,50 @@ import secondImage from "/public/vacation2.png";
 
 function Gallery() {
   return (
-    <article className="mb-10 grid grid-cols-6">
+    <article className="col-start-3 mb-10 grid grid-cols-4">
       <div className="col-span-2 row-start-1 flex items-end">
         <h3 className="mb-2 text-lg font-bold leading-10">Галерия</h3>
       </div>
       <div className="col-span-full row-start-2 grid grid-cols-4 gap-4">
         <div className="col-start-1 col-end-3 overflow-hidden">
           <Image
-            height={158}
-            width={294}
             src={firstImage}
             alt="Панорамна снимка на хотел"
-            className="transform transition-transform duration-300 ease-in-out hover:scale-105"
+            className="w-full transform transition-transform duration-300 ease-in-out hover:scale-105"
           />
         </div>
-        <div className="col-span-2 col-start-3 flex -translate-x-11 gap-4">
-          <div className="overflow-hidden">
-            <Image
-              className="transform transition-transform duration-300 ease-in-out hover:scale-105"
-              src={secondImage}
-              alt="Снимка на културно наследство в Йордания"
-            />
-          </div>
-          <div className="overflow-hidden">
-            <Image
-              className="transform transition-transform duration-300 ease-in-out hover:scale-105"
-              src={secondImage}
-              alt="Снимка на културно наследство в Йордания"
-            />
-          </div>
+        <div className="overflow-hidden">
+          <Image
+            className="h-full w-full transform transition-transform duration-300 ease-in-out hover:scale-105"
+            src={secondImage}
+            alt="Снимка на културно наследство в Йордания"
+          />
         </div>
-        <div className="col-start-1 col-end-3 flex gap-4 overflow-hidden">
-          <div className="overflow-hidden">
-            <Image
-              className="transform transition-transform duration-300 ease-in-out hover:scale-105"
-              src={secondImage}
-              alt="Снимка на културно наследство в Йордания"
-            />
-          </div>
-          <div className="overflow-hidden">
-            <Image
-              className="transform transition-transform duration-300 ease-in-out hover:scale-105"
-              src={secondImage}
-              alt="Снимка на културно наследство в Йордания"
-            />
-          </div>
+        <div className="overflow-hidden">
+          <Image
+            className="h-full w-full transform transition-transform duration-300 ease-in-out hover:scale-105"
+            src={secondImage}
+            alt="Снимка на културно наследство в Йордания"
+          />
+        </div>
+        <div className="overflow-hidden">
+          <Image
+            className="h-full w-full transform transition-transform duration-300 ease-in-out hover:scale-105"
+            src={secondImage}
+            alt="Снимка на културно наследство в Йордания"
+          />
+        </div>
+        <div className="overflow-hidden">
+          <Image
+            className="h-full w-full transform transition-transform duration-300 ease-in-out hover:scale-105"
+            src={secondImage}
+            alt="Снимка на културно наследство в Йордания"
+          />
         </div>
         <div className="col-start-3 col-end-5 overflow-hidden">
           <Image
-            height={158}
-            width={294}
             src={firstImage}
-            className="-translate-x-1 transform transition-transform duration-300 ease-in-out hover:scale-105"
+            className="w-full -translate-x-1 transform transition-transform duration-300 ease-in-out hover:scale-105"
             alt="Панорамна снимка на хотел"
           />
         </div>
