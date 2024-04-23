1409f303c16d3acb628041abc7853a6c13323b99
didoslavov <didkoslawow@gmail.com>
2024-04-23 05:00:38 +0300

    add responsive styles

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -10,8 +10,8 @@ import Program from "./Program/Program";
 function Content() {
   return (
     <div className="col-span-full grid grid-cols-12">
-      <section className="col-start-2 col-end-8 ml-32  pr-2 pt-24">
-        <div className="w-[85%] pl-16">
+      <section className="col-span-full md:col-start-2 md:col-end-12 lg:col-start-1 lg:col-end-8 2xl:col-start-2 2xl:col-end-8 2xl:ml-32  2xl:pr-2 2xl:pt-24">
+        <div className="px-6 py-6 sm:px-12 md:py-12  2xl:pl-16">
           <About />
           <Gallery />
           <Promos />
