da2cd10f5f2c6a7196d0217a65da3abb4722e481
didoslavov <didkoslawow@gmail.com>
2024-04-23 17:07:32 +0300

    fix responsive styles

--- src/components/Footer/Contacts.tsx
+++ src/components/Footer/Contacts.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Contacts() {
   return (
-    <article className="col-span-2 col-start-5 self-center ps-2 pt-10">
-      <h3 className="mb-7 text-[17px] font-extrabold leading-[25px]">
+    <article className="col-span-full col-start-3 md:col-span-4 md:col-start-7 lg:col-start-8 xl:col-span-3 xl:col-start-4 xl:self-center xl:ps-2 xl:pt-10 2xl:col-span-2 2xl:col-start-5">
+      <h3 className="mb-4 text-[17px] font-extrabold leading-[25px] md:mb-7">
         Контакти
       </h3>
       <ul className="flex flex-col gap-2 text-[17px]">
